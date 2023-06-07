778a1bc91437e2feb4366c86b1cc1daa5d79ec0f
Hamim21114395 <hamimnurkhamid@students.amikom.ac.id>
2023-06-07 19:14:52 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from './components/Tugas/NavBar.vue'
+import HeroSection from './components/Tugas/HeroSection.vue'
+import ProductSection from './components/Tugas/ProdukSection.vue'
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+  <HeroSection />
+  <ProductSection />
+  <main></main>
+</template>
